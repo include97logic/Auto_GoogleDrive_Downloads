f42ba3a518f1b7d1f7dcfde6645d65233e2fd760
GAJENDRA <38622189+include97logic@users.noreply.github.com>
2022-02-18 10:09:23 +0530

    Add files via upload

--- auto_download.py
+++ auto_download.py
@@ -2,7 +2,12 @@ import webbrowser
 import logging
 import argparse
 import pandas as pd
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
+Authenticate = GoogleAuth()
+Authenticate.LocalWebserverAuth()
+drive = GoogleDrive(Authenticate)
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -44,19 +49,52 @@ class AutoDownload(object):
             self.initiate_download(i)
         return readable_url, bool(dict_info_tst)
 
+    @staticmethod
+    def initiate_folder_download(url):
+        """ Initiates download for all files of the given folder URL(Private/Public)
+            :param url
+        """
+        data = {}
+        logging.info('Downloading all the contents of given folder')
+        folder_id = (url.split('/')[-1]).replace('?usp=sharing', '')
+        file_lst = drive.ListFile(
+            {'q': "'{}' in parents and trashed=false".format(folder_id)}).GetList()
+        for i in file_lst:
+            data['id'] = i['id']
+            fil_obj = drive.CreateFile(data)
+            fil_obj.GetContentFile(i['title'])
+            data.clear()
+
+    def initiate_pvt_file_download(self, url):
+        """ Initiates download for given private file URL
+        :param url[0] URL
+        :param url[1] File Name
+        """
+        data = {}
+        data['id'] = self.get_file_id(url[0])
+        fil_obj = drive.CreateFile(data)
+        fil_obj.GetContentFile(url[1])
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Script to download file/files from Google Drive")
     parser.add_argument("--file_link", help="link to file on Drive")
     parser.add_argument("--spreadsheet", help="Link to Spreadsheet on Google drive consisting of "
                         "details of files & links")
+    parser.add_argument("--folder_link", help="link to Folder (Either shared or Private) on Drive")
+    parser.add_argument("--private_file_link", nargs=2, help="link to private file on Drive")
     parser.add_argument("--test", default=False, help="Initiates test")
+
     args = parser.parse_args()
     obj = AutoDownload()
     if args.file_link is not None:
         obj.initiate_download(args.file_link)
     if args.spreadsheet is not None:
         obj.read_checkout_xl(args.spreadsheet)
+    if args.folder_link is not None:
+        obj.initiate_folder_download(args.folder_link)
+    if args.private_file_link is not None:
+        obj.initiate_pvt_file_download(args.private_file_link)
     if args.test == 'True':
         logging.info('Testing Method: get_file_id')
         ret_id = obj.get_file_id('https://drive.google.com/file/d/1ELFoAY5e423yt0PgzzBVlspEDy7TUhke/view?usp=sharing')
@@ -65,4 +103,3 @@ if __name__ == "__main__":
         url_val = obj.read_checkout_xl("https://docs.google.com/spreadsheets/d/1ZmjlFlHx-RY19apRQt1mUUg2i-hSti2iFNxLT96Pxws/edit?usp=sharing")
         assert '/export?format=csv&gid=0' in url_val[0], "Generated URL is not Readable by Pandas Please check the url"
         assert url_val[1]is True, "Dictionary object is Empty, Unable to Extract Data"
-
